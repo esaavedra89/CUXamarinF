341c1cc89a9b73ad8176d36e20dd77a5f6815820
neosaavedra <esaavedra@neotecnologias.com>
2019-12-28 10:33:36 -0400

    routing effect

--- CUXamarin/CUXamarin/Effects/EffectDemo.xaml.cs
+++ CUXamarin/CUXamarin/Effects/EffectDemo.xaml.cs
@@ -7,15 +7,17 @@ namespace CUXamarin.Effects
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EffectDemo : ContentPage
     {
-        Effect sliderEffect;
+        //Effect sliderEffect;
+        RoutingEffect sliderEffect;
 
         public EffectDemo()
         {
             InitializeComponent();
 
             // Aquí buscamos el efecto que esté bajo este nombre.
-            sliderEffect = Effect.Resolve("ES.SimpleSliderEffect");
+            //sliderEffect = Effect.Resolve("ES.SimpleSliderEffect");
 
+            sliderEffect = new SliderEffect(); // Este acepta.
             //sliderEffect = new SliderEffect();
             mySlider.Effects.Add(sliderEffect);// Lo agregamos a la lista de efectos.
             // Si no encuentra un efecto con este nombre, no habrá ningun error
---
+++ CUXamarin/CUXamarin/Effects/SliderEffect.cs
@@ -0,0 +1,16 @@
+﻿
+using Xamarin.Forms;
+
+namespace CUXamarin.Effects
+{
+    public class SliderEffect : RoutingEffect
+    {
+        /// <summary>
+        /// esta clase la podemos usar sin necesidad de reolverlo por otro lado.
+        /// </summary>
+        public SliderEffect() : base("ES.SimpleSliderEffect")
+        {
+
+        }
+    }
+}
